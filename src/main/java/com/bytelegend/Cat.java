5e8d5ddb4f6dc93e3119c8e7f5219b994b8f30a6
SaraadKun <35584589+SaraadKun@users.noreply.github.com>
2022-07-31 11:47:22 +0000

    Update at 2022-07-31T11:47:22.461497Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -10,8 +10,12 @@ public class Cat extends Pet {
         this.age = age;
     }
 
-    private String buildDescription() {
+    protected String buildDescription() {
         return "I am a cat named " + getName() + " and I am " + age + " years old";
+    }
+
+    public String getDesc() {
+        return desc;
     }
 
     @Override
