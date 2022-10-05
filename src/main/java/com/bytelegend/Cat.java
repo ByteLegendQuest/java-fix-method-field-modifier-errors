7442f4d298acc7e9a43aa253656cf08f7d3db41c
Anlyui <1391469813@qq.com>
2022-10-05 12:46:29 +0000

    Update at 2022-10-05T12:46:29.386633Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -10,12 +10,12 @@ public class Cat extends Pet {
         this.age = age;
     }
 
-    private String buildDescription() {
-        return "I am a cat named " + name + " and I am " + age + " years old";
+    public String buildDescription() {
+        return "I am a cat named " + super.getName() + " and I am " + age + " years old";
     }
 
     @Override
-    protected void describeMyself() {
+    public void describeMyself() {
         System.out.println(desc);
     }
 }
