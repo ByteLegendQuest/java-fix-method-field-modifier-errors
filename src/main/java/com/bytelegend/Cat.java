30cd3aca7c6e2c73365132a30c45988fe79462b3
ov0o0vo <3035566793@qq.com>
2023-03-02 05:39:27 +0000

    Update at 2023-03-02T05:39:27.511111Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -11,7 +11,7 @@ public class Cat extends Pet {
     }
 
     protected String buildDescription() {
-        return "I am a cat named " + getName + " and I am " + age + " years old";
+        return "I am a cat named " + getName() + " and I am " + age + " years old";
     }
 
     @Override
